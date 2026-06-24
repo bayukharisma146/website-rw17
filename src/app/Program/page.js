3fd45650f6baee04e21a62634416e0a202ad797f
Bayu Kharisma <kelvinpandu19@gmail.com>
2026-06-24 17:57:28 +0700

    Update website

---
+++ src/app/Program/page.js
@@ -0,0 +1,14 @@
+"use client";
+
+import { useState } from "react";
+import Navbar from "@/components/Navbar";
+import GalleryCard from "@/components/GalleryCard";
+import GalleryModal from "@/components/GalleryModal";
+
+export default function Program() {
+  return (
+    <main>
+      <h1>Program Kerja RW 17</h1>
+    </main>
+  );
+}
